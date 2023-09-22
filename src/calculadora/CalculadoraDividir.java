6a3ba6621b63abea7be17e94a4704a7871ab765d
Vanessa Cristina <vanessa.cristina.ads@gmail.com>
2023-09-22 20:52:08 -0300

    First commit

--- src/calculadora/CalculadoraDividir.java
+++
@@ -1,8 +0,0 @@
-package calculadora;
-
-public class CalculadoraDividir {
-
-    public static double dividir(double a, double b) {
-        return a / b;
-    }
-}
--- src/calculadora/CalculadoraSomar.java
+++
@@ -1,7 +0,0 @@
-package calculadora;
-
-public class CalculadoraSomar {
-    public static double somar(double a, double b) {
-        return a + b;
-    }
-}
